72905103ba5ea2376cf54b4b878d326ace5b27d6
owaisnm <owaisn92@gmail.com>
2018-11-18 19:05:48 -0500

    finished Prob46; added Prob47

--- src/Prob46.java
+++ src/Prob46.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Prob46 {
 
+    public List<List<Integer>> permute(int[] nums) {
+    	
+    	List<List<Integer>> solutionList = new ArrayList<List<Integer>>();
+    	backtrack(solutionList, nums, new ArrayList<>());
+    	return solutionList;
+    }
+   
+    public void backtrack(List<List<Integer>> solutionList, int[] nums, List<Integer> sol) {
+    	
+    	if(sol.size() == nums.length) {
+    		solutionList.add(new ArrayList<>(sol));
+    	} else {
+	    	for(int i = 0; i < nums.length; i++) {
+	    		if(sol.contains(nums[i])) {
+	    			continue;
+	    		}
+	    		sol.add(nums[i]);
+	    		backtrack(solutionList, nums, sol);
+	    		sol.remove(sol.size()-1);
+	    	}
+    	}
+    }
 }
---
+++ src/Prob47.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Prob47 {
+	
+    public List<List<Integer>> permute(int[] nums) {
+    	
+    	List<List<Integer>> solutionList = new ArrayList<List<Integer>>();
+    	boolean visited[] = new boolean[nums.length];
+    	Arrays.sort(nums);
+    	backtrack(solutionList, nums, new ArrayList<>(), visited);
+    	return solutionList;
+    }
+   
+    public void backtrack(List<List<Integer>> solutionList, int[] nums, List<Integer> sol, boolean visited[] ) {
+    	
+    	if(sol.size() == nums.length) {
+    		solutionList.add(new ArrayList<>(sol));
+    	} else {
+	    	for(int i = 0; i < nums.length; i++) {
+	    		if(visited[i] || i > 0 && nums[i-1] == nums[i] && !visited[i-1]) {
+	    			continue;
+	    		}
+	    		sol.add(nums[i]);
+	    		visited[i] = true;
+	    		backtrack(solutionList, nums, sol, visited);
+	    		visited[i] = false;
+	    		sol.remove(sol.size()-1);
+	    	}
+    	}
+    }
+}
