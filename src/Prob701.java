47f3d41102da0a3b82518bc77e172d181d81a0cd
Owais Naeem <owaisn92@gmail.com>
2019-03-03 19:05:18 -0500

    prob701

---
+++ src/Prob701.java
@@ -0,0 +1,31 @@
+import Prob654.TreeNode;
+
+public class Prob701 {
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	
+	   public TreeNode insertIntoBST(TreeNode root, int val) {
+	        return insert(root, val);
+	    }
+	    
+	    TreeNode insert(TreeNode root, int val) {
+	        if(root.val < val) {
+	            if(root.right != null) {
+	                insert(root.right, val);
+	            } else {
+	                root.right = new TreeNode(val);
+	            }
+	        } else {
+	            if(root.left != null) {
+	                insert(root.left, val);
+	            } else {
+	                root.left = new TreeNode(val);
+	            }
+	        }
+	        return root;
+	    }
+}
