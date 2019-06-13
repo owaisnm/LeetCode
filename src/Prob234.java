d089663268610594be53025698201529886772d8
Owais Naeem <owaisn92@gmail.com>
2019-06-13 18:46:44 -0400

    addd optimal solution with O(1) space for Prob234

--- src/Prob234.java
+++ src/Prob234.java
@@ -7,7 +7,7 @@ public class Prob234 {
 		      ListNode next;
 		      ListNode(int x) { val = x; }
 		  }
-	
+
 	// space: O(n), time: O(n)
 	  public boolean isPalindrome(ListNode head) {
 	        List<Integer> list = new ArrayList<>();
@@ -25,7 +25,48 @@ public class Prob234 {
 	        }
 	        return true;
 	    }
-//	  public boolean isPalindromeOptimal(ListNode head) {
-
+	  
+	// space: O(1), time: O(n)
+	  public boolean isPalindromeOptimal(ListNode head) {
+	        if( head == null || head.next == null) {
+	            return true;
+	        }
+	            
+	        // identify halfway
+	        ListNode slowCursor, fastCursor;
+	        slowCursor = head;
+	        fastCursor = head.next;
+	        while(fastCursor.next != null && fastCursor.next.next != null) {
+	            slowCursor = slowCursor.next;
+	            fastCursor = fastCursor.next.next;
+	        }
+	        if(fastCursor.next != null) {
+	            slowCursor = slowCursor.next;
+	            fastCursor = fastCursor.next;
+	        }
+	        
+	        // reverse 2nd half
+	        ListNode curr, next, prev;
+	        curr = slowCursor;
+	        prev = next = null;
+	        while(curr != null) {
+	            next = curr.next;
+	            curr.next = prev;
+	            prev = curr;
+	            curr = next;
+	        }
+	        
+	        // prev is now the last node
+	        // we need curr to be the first
+	        curr = head;
+	        while(curr != null) {
+	            if(curr.val != prev.val) {
+	                return false;
+	            } 
+	            curr = curr.next;
+	            prev = prev.next;
+	        }
+	        return true;
+	    } 
 	 
 }
