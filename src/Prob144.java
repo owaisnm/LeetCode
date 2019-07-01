8ae5a70976ed919bc62fb79efdb071650a40841b
Owais Naeem <owaisn92@gmail.com>
2019-07-01 15:03:23 -0400

    added LCA solution using recursion

--- src/Prob144.java
+++ src/Prob144.java
@@ -1,4 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Prob144 {
-
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	
+	public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return list;
+        }
+        // preOrderRecursive(root, list);
+        preOrderIterative(root, list);
+        return list;
+    }
+    
+    // using recursion
+    public void preOrderRecursive(TreeNode root, List<Integer> list) {
+        if(root == null ) {
+            return;
+        } else {
+            list.add(root.val);
+        }
+        preOrderRecursive(root.left, list);
+        preOrderRecursive(root.right, list);
+    }
+    
+    // using a stack to implement DFS
+    public void preOrderIterative(TreeNode root, List<Integer> list) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode cursor;
+        while(!stack.isEmpty()) {
+            cursor = stack.pop();
+            list.add(cursor.val);
+            if(cursor.right != null) {
+                stack.add(cursor.right);
+            }
+            if(cursor.left != null) {
+                stack.add(cursor.left);
+            }       
+        }
+    }
 }
---
+++ src/Prob236.java
@@ -0,0 +1,23 @@
+
+public class Prob236 {
+	public class TreeNode {
+		      int val;
+		     TreeNode left;
+		     TreeNode right;
+		     TreeNode(int x) { val = x; }
+		 }
+	   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+	        if(root == null || root.val == p.val || root.val == q.val) {
+	            return root;
+	        }
+	        
+	        TreeNode left = lowestCommonAncestor(root.left, p, q);
+	        TreeNode right = lowestCommonAncestor(root.right, p, q);
+	        
+	        if(left != null && right != null) {
+	            return root;
+	        }
+	        
+	        return left != null ? left : right; 
+	    }
+}
